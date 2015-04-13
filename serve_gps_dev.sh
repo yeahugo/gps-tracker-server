1561f934252909b5d76ff95b83bfb755b6fb6b4a
yeahugo <yjhuestc@aliyun.com>
2015-04-13 09:20:14 +0800

    update start server shell

--- serve_gps_dev.sh
+++ serve_gps_dev.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 SESSION_NAME=gps_serve
-
+PATH = gps-tracker-server
 tmux has-session -t ${SESSION_NAME} 2>/dev/null
 if [ $? != 0 ] ; then
     tmux new-session -s ${SESSION_NAME} -d
 
-    tmux send-keys -t ${SESSION_NAME} 'cd ~/ws/gps/www' C-m
-    tmux send-keys -t ${SESSION_NAME} 'python -m SimpleHTTPServer 9898' C-m
+#    tmux send-keys -t ${SESSION_NAME} 'cd ~/ws/gps/www' C-m
+#    tmux send-keys -t ${SESSION_NAME} 'python -m SimpleHTTPServer 9898' C-m
 
     tmux split-window -t ${SESSION_NAME}
-    tmux send-keys -t ${SESSION_NAME} 'cd ~/ws/gps' C-m
+    tmux send-keys -t ${SESSION_NAME} 'cd gps-tracker-server' C-m
     tmux send-keys -t ${SESSION_NAME} '. venv/gps/bin/activate' C-m
-    tmux send-keys -t ${SESSION_NAME} './device_gateway.py' C-m
+    tmux send-keys -t ${SESSION_NAME} 'nohup python device_gateway.py' C-m
 
     tmux split-window -t ${SESSION_NAME}
-    tmux send-keys -t ${SESSION_NAME} 'cd ~/ws/gps' C-m
+    tmux send-keys -t ${SESSION_NAME} 'cd ~/gps-tracker-server' C-m
     tmux send-keys -t ${SESSION_NAME} '. venv/gps/bin/activate' C-m
-    tmux send-keys -t ${SESSION_NAME} './rest_api/rest_api.py' C-m
+    tmux send-keys -t ${SESSION_NAME} 'gunicorn -c gunicorn.conf rest_api:app' C-m
 
     tmux split-window -t ${SESSION_NAME}
-    tmux send-keys -t ${SESSION_NAME} 'cd ~/ws/gps/www' C-m
+    tmux send-keys -t ${SESSION_NAME} 'cd ~/gps-tracker-server/www' C-m
     tmux send-keys -t ${SESSION_NAME} 'gulp watch' C-m
 
 fi
